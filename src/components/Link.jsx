6f4763a4b7e29104c13facff2003e7cac093da10
Samuel Ajala <samuelajala01@gmail.com>
2022-10-30 23:49:06 +0000

    chore: add logos

--- src/components/Link.jsx
+++
@@ -1,9 +0,0 @@
-const Link = (props) => {
-    return (
-        <a href={props.href}>
-            <button id={props.id}>{props.name}</button>
-        </a>
-    )
-}
-
-export default Link
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,24 +1,30 @@
-import Link from "./Link"
-import TweetPic from "../assets/70098418.png" 
+import TweetPic from "../assets/70098418.png"
 import SlackLogo from "../assets/slack.svg"
 import GitLogo from "../assets/Icon.svg"
+import ZuriLogo from "../assets/zuri-internship.svg"
+import i4g from "../assets/I4G.svg"
+import shareDesktop from '../assets/share-btn-desktop.svg'
+import shareMobile from '../assets/share-btn-mobile.svg'
 
 const MainPage = () => {
-    return(
-        <>
-          <img src={TweetPic} className='tweet__pic' id="profile__img"/>
-          <p id="twitter">samuelajala01</p>
-          <span id="slack" hidden>Samuel Ajala</span>
-          <section>
-          <Link id='btn__zuri' href='https://training.zuri.team' name="Zuri Team" />
-          <Link id='books' href='https://books.zuri.team' name="Zuri Books" />
-          <Link id='book__python' href='https://books.zuri.team/python-for-beginners?ref_id=myslackusername' name="Python Books" />
-          <Link id='pitch' href='https://background.zuri.team' name="Background Check" />
-          <Link id='book__design' href='https://books.zuri.team/design-rules' name="Books on Design" />
-          </section>
-          <span className="logo__div"><img src={SlackLogo}/><img src={GitLogo} /></span>
-        </>
-    )
+  return (
+    <>
+      <img src={TweetPic} className='tweet__pic' id="profile__img" />
+      <img src={shareDesktop} className='shareDesktop' />
+      <img src={shareMobile} className='shareMobile' />
+      <p id="twitter">samuelajala01</p>
+      <span id="slack" hidden>Samuel Ajala</span>
+      <section>
+        <a id='btn__zuri' href='https://training.zuri.team'>Zuri Team</a>
+        <a id='books' href='https://books.zuri.team'>Zuri Books</a>
+        <a id='book__python' href='https://books.zuri.team/python-for-beginners?ref_id=cy63rx'>Python Books</a>
+        <a id='pitch' href='https://background.zuri.team'>Background check for coders</a>
+        <a id='book__design' href='https://books.zuri.team/design-rules' >Design Books</a>
+      </section>
+      <span className="logo__div"><img src={SlackLogo} /><img src={GitLogo} /></span>
+      <footer><img src={ZuriLogo} /> <p>HNG Internship 9 Frontend task</p><img src={i4g} /></footer>
+    </>
+  )
 }
 
 export default MainPage
