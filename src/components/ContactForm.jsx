b55669badd743bc7b557bd42b2056af6100bd554
Samuel Ajala <samuelajala01@gmail.com>
2022-11-04 17:53:11 +0000

    chore: fix input width

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,10 +1,12 @@
+
+
 const ContactForm = () => {
     return (
         <form>
             <div className="row">
                 <div class="input-group">
                     <label for="first-name"> First Name</label>
-                    <input type="text" id="name" name="name" required />
+                    <input type="text" id="firstname" name="firstname" size='100%' required />
                 </div>
                 <div class="input-group">
                     <label for="last-name">Last Name</label>
@@ -12,9 +14,9 @@ const ContactForm = () => {
                 </div>
             </div>
             <label>Email</label>
-            <input type="mail" />
+            <input type="text" id="email" />
             <label>Message</label><textarea rows='10' />
-            <button>Send Message</button>
+            <button type="submit" value="submit">Send Message</button>
         </form>
     )
 }
