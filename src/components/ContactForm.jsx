8bf0069a4746d197f63c82fe913e123df08bf8ab
Samuel Ajala <samuelajala01@gmail.com>
2022-11-05 02:15:49 +0000

    chore: add simple form validation

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -6,6 +6,7 @@ const ContactForm = () => {
                 <div class="input-group">
                     <label for="first-name"> First Name</label>
                     <input type="text" id="first_name" name="firstname" placeholder="Enter your first name" required />
+
                 </div>
                 <div class="input-group">
                     <label for="last-name">Last Name</label>
@@ -14,13 +15,14 @@ const ContactForm = () => {
             </div>
             <div className="email">
                 <label>Email</label>
-                <input type="text" id="email" placeholder="yourname@email.com" />
+                <input type="email" id="email" placeholder="yourname@email.com" required />
             </div>
             <div className="message">
-                <label>Message</label><textarea rows='10' id="message" placeholder="Send me a message and I'll reply you as soon as possible..." />
+                <label>Message</label><textarea rows='10' id="message" placeholder="Send me a message and I'll reply you as soon as possible..." required />
+                <p className="message_info">Please enter a message</p>
             </div>
             <div className="contract">
-                <input type="checkbox" /><span>To agree to providing your data to {name} who may contact you</span>
+                <input type="checkbox" id="checkbox" required /><span>To agree to providing your data to {name} who may contact you</span>
             </div>
             <button type="submit" value="submit">Send Message</button>
         </form>
