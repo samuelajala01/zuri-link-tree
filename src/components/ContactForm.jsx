fd2f424cba3ae4650f98693efed119016129b525
Samuel Ajala <samuelajala01@gmail.com>
2022-11-05 20:33:39 +0000

    chore: change lable name

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -45,24 +45,24 @@ const ContactForm = () => {
         <form onSubmit={handleSubmit}>
             <div className="row">
                 <div className="input-group">
-                    <label htmlFor="first-name"> First Name</label>
+                    <label htmlFor="firstname"> First Name</label>
                     <input type="text" id="first_name" name="firstname" placeholder="Enter your first name" value={formValues.firstname} onChange={handleChange} />
                     <p className="error-info">{formErrors.firstname}</p>
                 </div>
                 <div className="input-group">
-                    <label htmlFor="last-name">Last Name</label>
+                    <label htmlFor="lastname">Last Name</label>
                     <input type="text" id="last_name" name="lastname" placeholder="Enter your last name" value={formValues.lastname} onChange={handleChange} />
                     <p className="error-info">{formErrors.lastname}</p>
                 </div>
             </div>
             <div className="email">
-                <label>Email</label>
+                <label htmlFor="email">Email</label>
                 <input type="email" id="email" name="email" placeholder="yourname@email.com" value={formValues.email} onChange={handleChange} />
                 <p className="error-info">{formErrors.email}</p>
             </div>
 
             <div className="message">
-                <label>Message</label><textarea rows='10' id="message" name="textarea" placeholder="Send me a message and I'll reply you as soon as possible..." value={formValues.textarea} onChange={handleChange} />
+                <label htmlFor="textarea">Message</label><textarea rows='10' id="message" name="textarea" placeholder="Send me a message and I'll reply you as soon as possible..." value={formValues.textarea} onChange={handleChange} />
                 <p className="error-info">{formErrors.textarea}</p>
             </div>
             <div className="contract">
